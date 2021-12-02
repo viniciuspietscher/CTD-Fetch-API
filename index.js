c79e3fe931848e327e2f145e5801844a9e5af666
Vinicius <vini.914@gmail.com>
2021-12-02 11:48:36 -0500

    add select planet

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-fetch('https://www.swapi.tech/api/planets/1')
+fetch('https://www.swapi.tech/api/planets/')
   .then(response => response.json())
-  .then(data => console.log(data.result.properties.name));
+  .then(data => {
+    data.results.forEach(element => {
+      console.log(element);
+      $(`<option value="${element.uid}"/>`).html(element.name).appendTo("#planets")
+    });
+  });
+
+const selectPlanet = () => {
+  const planet_id = $("#planets")[0].value;
+  fetch(`https://www.swapi.tech/api/planets/${planet_id}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.result.properties);
+    });
+}
+
