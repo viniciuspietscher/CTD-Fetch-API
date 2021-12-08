a3b044954dcb852a7671cbab270234fa119611ff
Vinicius Pietscher <vini.914@gmail.com>
2021-12-08 17:21:51 -0500

    show planet information

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ fetch('https://www.swapi.tech/api/planets/')
   .then(response => response.json())
   .then(data => {
     data.results.forEach(element => {
-      console.log(element);
+      // console.log(element);
       $(`<option value="${element.uid}"/>`).html(element.name).appendTo("#planets")
     });
   });
@@ -12,7 +12,14 @@ const selectPlanet = () => {
   fetch(`https://www.swapi.tech/api/planets/${planet_id}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data.result.properties);
+      document.getElementById('planet_name').textContent = `Planet name: ${data.result.properties.name}`
+      document.getElementById('planet_diameter').textContent = `Planet diameter: ${data.result.properties.diameter}`
+      document.getElementById('planet_rotation_period').textContent = `Planet rotation period: ${data.result.properties.rotation_period}`
+      document.getElementById('planet_orbital_period').textContent = `Planet orbital period: ${data.result.properties.orbital_period}`
+      document.getElementById('planet_population').textContent = `Planet population: ${data.result.properties.population}`
+      document.getElementById('planet_climate').textContent = `Planet climate: ${data.result.properties.climate}`
+      document.getElementById('planet_terrain').textContent = `Planet terrain: ${data.result.properties.terrain}`
+      document.getElementById('planet_surface_water').textContent = `Planet surface water: ${data.result.properties.surface_water}`
     });
 }
 
